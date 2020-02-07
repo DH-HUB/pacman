46bb9698b1956a39542760962e94bfa1141b0095
hakima <hakima>
2020-02-07 09:26:58 +0100

    demarrage projet

--- js/doc.js
+++ js/doc.js
@@ -24,23 +24,30 @@ var grille=[
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 
 ];
-function initGrille(){
 
-    let _grille= document.querySelector("#grille")
-    _grille.innerHTML="";
-    /*_grille.style.gridTemplateColumns=grille[0].length;
-    _grille.style.gridTemplateRows=grille.length;*/
-    _grille.style="grid-template-columns:repeat("+grille[0].length+",40px);grid-template-rows:repeat("+grille.length+",40px)";
-
-    for(let ligne in grille){
-console.log(grille[ligne])
-for(let col in grille){
-    console.log(grille[ligne][col])
-        }
-
+function initGrille() {
+    let _grille = document.querySelector('#grille');
+    _grille.innerHTML = "";
+    _grille.style.gridTemplateColumns = 'repeat('+grille[0].length+', 40px)';
+    _grille.style.gridTemplateRows = 'repeat('+grille.length+', 40px)';
+ 
+     for(let ligne in grille) {
+             grille[ligne];
+         
+         for(let col in grille[ligne]) {
+            let cellule = grille[ligne][col];
+            let monElem = document.createElement("div");
+            console.log(cellule);
+            if(cellule == 0) {
+             monElem.classList.add("mur");
+         } else if(cellule == 1) {
+             monElem.classList.add("sol");
+         } else if(cellule == 2) {
+             monElem.classList.add("bonbon");
+         }
+             _grille.append(monElem);
+         };
+     };
     }
-    
-}
 
-
-initGrille();
+    initGrille();
